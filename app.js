b6fec375b989bd49a4e377a00421a5557016e572
hasibur14 <hasib141415@gmail.com>
2024-02-29 12:22:24 +0600

    added banner section

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert()
+const btnContainer = document.getElementById('btn-container');
+
+const fetchCatagories = () =>{
+    const url = 'https://openapi.programming-hero.com/api/videos/categories'
+    fetch(url)
+    .then((res) => res.json())
+    .then(({data}) => {
+        data.forEach((card) => {
+            console.log(card)
+
+            const newBtn = document.createElement('button');
+            newBtn.classList = 'btn btn-ghost bg-slate-500 text-white text-lg'
+            newBtn.innerText = card.category;
+            newBtn.addEventListener('click', () => fetchDataByCatagories())
+            btnContainer.appendChild(newBtn)
+        })
+    })
+}
+
+
+const fetchDataByCatagories = (catagoriesID) =>{
+    console.log(catagoriesID)
+}
+fetchCatagories()
